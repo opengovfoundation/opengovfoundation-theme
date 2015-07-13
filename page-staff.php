d11c58ac4ba54f01ef991685466936dc7c7e1912
Bill Hunt <bill@krues8dr.com>
2015-07-13 16:22:48 -0400

    Separating Board, Staff, and Advisory Board.

--- page-staff.php
+++ page-staff.php
@@ -30,10 +30,13 @@ get_header(); ?>
 						<?php the_content(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 
+						<h1>Staff</h1>
 						<!-- Get all pages beneath this one -->
 						<?php $query = new WP_Query( array(
 							'post_parent' => $post->ID,
-							'post_type' => 'page'
+							'post_type' => 'page',
+							'meta_key' => 'type',
+							'meta_value' => 'Staff'
 						) );?>
 
 						<?php /* Check to see if we have results */ ?>
@@ -76,6 +79,109 @@ get_header(); ?>
 						<?php endif; ?>
 
 						<?php wp_reset_postdata(); ?>
+
+
+						<h1>Board of Directors</h1>
+						<!-- Get all pages beneath this one -->
+						<?php $query = new WP_Query( array(
+							'post_parent' => $post->ID,
+							'post_type' => 'page',
+							'meta_key' => 'type',
+							'meta_value' => 'Board'
+						) );?>
+
+						<?php /* Check to see if we have results */ ?>
+						<?php if ( $query->have_posts() ) : ?>
+							<section class="staff-list">
+								<?php while ( $query->have_posts() ) : ?>
+									<?php $query->the_post(); ?>
+
+									<div class="person">
+										<h2><?php print get_the_title() ?></h2>
+										<h3><?php print get_post_meta($post->ID, 'position', true); ?></h3>
+									</div>
+								<?php endwhile; ?>
+							</section>
+							<?php /* Rewind our query to the beginning */ ?>
+							<?php $query->rewind_posts() ?>
+
+							<!-- Details about each person for popup -->
+							<section class="staff-details" style="display: none">
+								<?php while ( $query->have_posts() ) : ?>
+									<?php $query->the_post(); ?>
+
+									<article class="person">
+										<?php if( has_post_thumbnail() ) : ?>
+										    <!-- show a 200x200px thumbnail -->
+											<?php print the_post_thumbnail( array(200,200) ); ?>
+										<?php endif; ?>
+										<h2><?php print the_title() ?></h2>
+										<h3><?php print get_post_meta($post->ID, 'position', true); ?></h3>
+										<section class="bio">
+											<?php print the_content(); ?>
+										</section>
+									</article>
+								<?php endwhile; ?>
+							</section>
+
+						<?php else: ?>
+							No staff found.
+
+						<?php endif; ?>
+
+						<?php wp_reset_postdata(); ?>
+
+
+						<h1>Advisory Board</h1>
+						<!-- Get all pages beneath this one -->
+						<?php $query = new WP_Query( array(
+							'post_parent' => $post->ID,
+							'post_type' => 'page',
+							'meta_key' => 'type',
+							'meta_value' => 'Advisory Board'
+						) );?>
+
+						<?php /* Check to see if we have results */ ?>
+						<?php if ( $query->have_posts() ) : ?>
+							<section class="staff-list">
+								<?php while ( $query->have_posts() ) : ?>
+									<?php $query->the_post(); ?>
+
+									<div class="person">
+										<h2><?php print get_the_title() ?></h2>
+										<h3><?php print get_post_meta($post->ID, 'position', true); ?></h3>
+									</div>
+								<?php endwhile; ?>
+							</section>
+							<?php /* Rewind our query to the beginning */ ?>
+							<?php $query->rewind_posts() ?>
+
+							<!-- Details about each person for popup -->
+							<section class="staff-details" style="display: none">
+								<?php while ( $query->have_posts() ) : ?>
+									<?php $query->the_post(); ?>
+
+									<article class="person">
+										<?php if( has_post_thumbnail() ) : ?>
+										    <!-- show a 200x200px thumbnail -->
+											<?php print the_post_thumbnail( array(200,200) ); ?>
+										<?php endif; ?>
+										<h2><?php print the_title() ?></h2>
+										<h3><?php print get_post_meta($post->ID, 'position', true); ?></h3>
+										<section class="bio">
+											<?php print the_content(); ?>
+										</section>
+									</article>
+								<?php endwhile; ?>
+							</section>
+
+						<?php else: ?>
+							No staff found.
+
+						<?php endif; ?>
+
+						<?php wp_reset_postdata(); ?>
+
 
 					</div><!-- .entry-content -->
 
