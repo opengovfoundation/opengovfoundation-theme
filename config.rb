59d1fb81d4f42369bfa616ba290e3eb59d2e2e06
John Athayde <jmpa@meticulous.com>
2015-07-17 12:32:29 -0400

    Setting it to overwrite root style.css file

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ add_import_path "bower_components/font-awesome/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = ""
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
