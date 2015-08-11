3d988513afcd677e8e8e38fc7784f705d0d4154f
Casey Gollan <hello@caseyagollan.com>
2015-08-11 09:12:49 -0400

    contact page improvements #3

--- contact.php
+++ contact.php
@@ -12,8 +12,8 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div class="container">
 			<div class="row padded-row">
-				<h1 class="col-md-6">Contact Us</h1>
-				<ul class="socialmedia col-md-6">
+				<h1 class="col-sm-6">Contact Us</h1>
+				<ul class="socialmedia col-sm-6">
 					<li><a href="https://twitter.com/FoundOpenGov" class="img-circle"><span class="genericon genericon-twitter"></span></a></li>
 					<li><a href="https://www.facebook.com/Opengovfoundation" class="img-circle"><span class="genericon genericon-facebook"></span></a></li>
 					<li><a href="https://www.youtube.com/channel/UCdSjbzPy92uMG8o03N1JC5g" class="img-circle"><span class="genericon genericon-youtube"></span></a></li>
@@ -25,27 +25,31 @@ get_header(); ?>
 		<div class="row padded-row map">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-12 col-md-7 col-md-offset-5 contact">
+					<div class="col-sm-12 col-md-8 col-md-offset-4 contact">
 						<div class="col-md-2 icon">
 							<img src="/wp-content/themes/opengovfoundation-theme/images/icon_contact_white.png" alt="">
 						</div>
-						<div class="col-md-5 col-sm-6">
+						<div class="col-md-4 col-sm-6">
 							<h3>Physical:</h3>
 							<p>1875 Connecticut Ave NW<br>
 							10th Floor<br>
-							Washington DC 20002<br>
+							Washington DC 20009<br>
 							<a href="https://www.google.com/maps/dir//1875+Connecticut+Ave+NW,+Washington,+DC+20009/">Map directions</a></p>
 						</div>
-						<div class="col-md-5 col-sm-6">
+						<div class="col-md-6 col-sm-6">
 							<h3>Nearest Metro:</h3>
-							<p>Dupont Circle — North</p>
-							<br>
-							<h3>Email:</h3>
-							<p><a href="mailto:sayhello@opengovfoundation.org">sayhello@opengovfoundation.org</a></p>
-							<h3>Phone:</h3>
-							<p>760.659.0631</p>
+							<p><span style="color:red;">●</span> Dupont Circle — North</p>
+							<h3 class="inline">Email:</h3>
+							<p class="inline"><a href="mailto:sayhello@opengovfoundation.org">sayhello@opengovfoundation.org</a></p><br class="small">
+							<h3 class="inline">Phone:</h3>
+							<p class="inline">760.659.0631</p>
 						</div>
 					</div>
+				</div>
+				<div class="row mobile-map">
+					<figure class="col-sm-12">
+						<img src="/wp-content/themes/opengovfoundation-theme/images/map.jpg">
+					</figure>
 				</div>
 			</div>
 		</div>
